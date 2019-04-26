6e854faaf1f7bfe12d0693bd37927378092df38a
HATATANI Shinta <gamaguchi@gmail.com>
2019-04-26 22:04:18 +0900

    Update file

--- main.cc
+++ main.cc
@@ -4,12 +4,15 @@
 #include <iostream>
 
 #include "zetasql/parser/parser.h"
-int main() {
-  std::string query_string("SELECT\n"
-                      "  *\n"
-                      "FROM\n"
-                      "  foo\n");
+
+std::string formatSql(std::string sql) {
   std::unique_ptr<zetasql::ParserOutput> parser_output;
-  std::string unparsed_string = zetasql::Unparse(parser_output->statement());
-  std::cout << unparsed_string;
+  zetasql::ParseStatement(sql, zetasql::ParserOptions(), &parser_output);
+  return zetasql::Unparse(parser_output->statement());
 }
+
+int main(int argc, char *argv[]) {
+  std::string query_string(argv[1]);
+  std::cout << formatSql(query_string);
+}
+
